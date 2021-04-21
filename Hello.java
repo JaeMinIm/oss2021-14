adfbf728c4491d3d0158996df80e2a736fd1568d
jaeminIm <n94n@naver.com>
2021-04-22 01:23:25 +0900

    Update Hello.java:kkk

--- Hello.java
+++ Hello.java
@@ -5,7 +5,11 @@ public class Hello{
 		system.out.print("KKK");
 		
 		int x = 10;
+<<<<<<< Updated upstream
 		system.out.println("KKKK");
 		
+=======
+		system.out.print("Hello newFunc");
+>>>>>>> Stashed changes
 	}}
 
